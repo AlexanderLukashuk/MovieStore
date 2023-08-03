6dbbc2affe05b7c8c847a29043359abb845a5c04
AlexanderLukashuk <upiter465@gmail.com>
2023-08-03 17:09:37 +0600

    Initial commit

---
+++ MovieStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
