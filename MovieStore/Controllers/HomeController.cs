ce9ba54e0f6edc59237e1d7c686c1308520fe9d7
AlexanderLukashuk <upiter465@gmail.com>
2023-08-03 17:24:52 +0600

    Created new controller

--- MovieStore/Controllers/HomeController.cs
+++ MovieStore/Controllers/HomeController.cs
@@ -1,32 +1,12 @@
-﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MovieStore.Models;
 
-namespace MovieStore.Controllers;
-
-public class HomeController : Controller
+namespace MovieStore.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
-
